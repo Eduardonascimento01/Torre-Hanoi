70a4ee071e10c9c7a0f08fae9d7120437b3b3656
edunasciment <edukbca11@gmail.com>
2026-05-04 19:05:38 -0300

    Forçando atualização do cache para o novo visual

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'torre-hanoi-v2';
+const CACHE_NAME = 'torre-hanoi-v4';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
